97ab1ff69143a8eb01ccb358f692e62ff42eb024
Juan Devsigner <54295937+juandevsigner@users.noreply.github.com>
2023-05-20 08:56:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 dotenv.config();
 conectDb();
 
-const whitelist = [process.env.FRONTEND_URL];
+const whitelist = [];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.includes(origin)) {
